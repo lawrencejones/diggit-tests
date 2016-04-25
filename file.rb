0a0544cd9bedf7e39cbd95cd494b740cd00c1201
Lawrence Jones <lawrjone@gmail.com>
2016-04-25 13:42:20 +0100

    .from_uri, bump Utils::Socket::initialize once more

--- file.rb
+++ file.rb
@@ -1,8 +1,14 @@
 module Utils
   class Socket
+    def self.from_uri(uri)
+      host, port = URI.parse(uri)
+      new(host, port)
+    end
+
     def initialize(host, port)
       @host = host
       @port = port
+      puts("Created new socket on #{host}:#{port}")
     end
   end
 end
