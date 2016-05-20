add33f98f097f01c44b0da3167f8f9175af93b03
The rugged tests are fragile <lawrjone@gmail.com>
2016-05-20 19:19:33 +0100

    Render @third, app_controller/app_template

--- app_controller.rb
+++ app_controller.rb
@@ -1,5 +1,8 @@
 class AppController
   def render_template
-    render('app_template.html', first: 'first', second: 'second')
+    render('app_template.html',
+           first: 'first',
+           second: 'second',
+           third: 'third')
   end
 end
